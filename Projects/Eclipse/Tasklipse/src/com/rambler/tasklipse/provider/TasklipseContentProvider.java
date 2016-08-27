c604406959c5285f8740a4e56c825b9bf1979925
vanditsramblings <vanditsharma001@gmail.com>
2016-08-27 21:01:25 +0530

    Updated project structure

---
+++ Projects/Eclipse/Tasklipse/src/com/rambler/tasklipse/provider/TasklipseContentProvider.java
@@ -0,0 +1,26 @@
+package com.rambler.tasklipse.provider;
+
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+public class TasklipseContentProvider implements IStructuredContentProvider{
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inputChanged(Viewer arg0, Object arg1, Object arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object[] getElements(Object list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
