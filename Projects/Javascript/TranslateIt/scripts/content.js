cc4624593b28c2d91d16883d4be255b3a76c6562
vanditsramblings <vanditsharma001@gmail.com>
2016-09-09 23:23:02 +0530

    Corrected positioning and added Yandex api calls

--- Projects/Javascript/TranslateIt/scripts/content.js
+++ Projects/Javascript/TranslateIt/scripts/content.js
@@ -1,44 +1,93 @@
-
-//Append popover to view
-var popover = document.createElement('div');
-popover.setAttribute('class', 'translateit-popover');
-document.body.appendChild(popover);
-
-
-//Shows popover
-function showPopover(mouseX, mouseY, selection) {
-  popover.innerHTML = selection;
-  popover.style.top = mouseY + 'px';
-  popover.style.left = mouseX + 'px';
-  popover.style.visibility = 'visible';
-}
-
-//Hides popover
-function hidePopover() {
-	popover.style.zIndex 	   = -1;
-	popover.style.visibility = 'hidden';	
-}
+/*
+*Translate It Plugin.
+*A popover based plugin , which displays the translated text in the popover 
+*as per the user preference.
+*
+*Powered by Yandex Translation API
+*Developed By Vandit Sharma(vanditsharma001@gmail.com)
+*/
 
 
-//Single Click callback : Hide popover
-clickCallback=function(){
-	hidePopover();
-}
+(function($) {
+	var key="trnsl.1.1.20160909T103949Z.cd2d55f0764e78cb.f598c8b4282d6d69834ac3273b72fa7de737ecbf";
+	var targetLang="ru";
+	//Append popover to view
+	var popover = document.createElement('div');
+	popover.setAttribute('class', 'translateit-popover');
+	document.body.appendChild(popover);
 
-//Double click callback : Show popover
-doubleClickCallback=function(){
-  	//Proceed only when it is a valid selection
-	if(window.getSelection()!=null && 
-	   window.getSelection()!= undefined && 
-	   window.getSelection().toString().trim()!="" ){
-		var selection = window.getSelection(),
-			range     = selection.getRangeAt(0),
-			rect      = range.getBoundingClientRect();
-		showPopover(rect.left,rect.top,selection);
+
+	//Shows popover
+	function showPopover(mouseX, mouseY, selection) {
+
+		if(mouseY<100 && mouseY<180 && mouseX >90){
+			popover.style.top  = (mouseY + window.scrollY) +'px';
+			popover.style.left = (mouseX + window.scrollX-90) + 'px';
+		}
+		else if(mouseY>100 && mouseY<180 && mouseX >90){
+			popover.style.top  = (mouseY + window.scrollY) +'px';
+			popover.style.left = (mouseX + window.scrollX-90) + 'px';
+		}
+		else if(mouseY<180 && mouseX <90){	
+			popover.style.top  = (mouseY + window.scrollY) +'px';
+			popover.style.left = (mouseX + window.scrollX-10) + 'px';
+		}else{
+			popover.style.top  = (mouseY + window.scrollY-180) +'px';
+			popover.style.left = (mouseX + window.scrollX-90) + 'px';
+		}
+		var translation=getTranslation(targetLang,selection.toString());
+		
 	}
-}
+	
+	getTranslation=function(targetLang,text){
+		$.ajax({
+		  method: "POST",
+		  url: "https://translate.yandex.net/api/v1.5/tr.json/detect",
+		  data: { key: key, text: text }
+		}).done(function(detectData) {
+			  $.ajax({
+			  method: "POST",
+			  url: "https://translate.yandex.net/api/v1.5/tr.json/translate",
+			  data: { key: key,"text to translate": text ,"translation direction":detectData.lang+"-"+targetLang}
+			}).done(function(data) {
+			  console.log(data);
+			  return data.text[0];
+			  popover.innerHTML=translation;
+			  popover.style.visibility = 'visible';
+		      popover.style.zIndex	 = 999;
+			});
+	  	});
+	}
 
-//Register Event Listener for Double click
-document.body.addEventListener('dblclick',doubleClickCallback);
 
-document.body.addEventListener('click',clickCallback);
+	//Hides popover
+	hidePopover=function() {
+		popover.style.zIndex 	   = -1;
+		popover.style.visibility = 'hidden';	
+	}
+
+
+	//Single Click callback : Hide popover
+	clickCallback=function(){
+		hidePopover();
+	}
+
+	//Double click callback : Show popover
+	doubleClickCallback=function(){
+		//Proceed only when it is a valid selection
+		if(window.getSelection()!=null && 
+		   window.getSelection()!= undefined && 
+		   window.getSelection().toString().trim()!="" ){
+			var selection = window.getSelection(),
+				range     = selection.getRangeAt(0),
+				rect      = range.getBoundingClientRect();
+			showPopover(rect.left,rect.top,selection);
+		}
+	}
+
+	//Register Event Listener for Double click
+	document.body.addEventListener('dblclick',doubleClickCallback);
+
+	document.body.addEventListener('click',clickCallback);
+
+})(jQuery);
