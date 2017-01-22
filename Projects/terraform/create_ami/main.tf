20cec5217f55f6f969145e82aefd0d338ae977af
vanditsramblings <vanditsharma001@gmail.com>
2017-01-22 18:08:38 +0530

    Terraform : Updated main module

--- Projects/terraform/create_ami/main.tf
+++ Projects/terraform/create_ami/main.tf
@@ -1,4 +1,3 @@
-
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
@@ -8,12 +7,13 @@ provider "aws" {
 module "instance_module" {
   source = "./modules/instance"
   aws_pem_file="${var.aws_pem_file}"
+  aws_key_name ="${var.aws_key_name}" 
 }
 
 
 module "ami_module" {
   source = "./modules/ami"
-  ami_name = "${var.ami_name}"
+  aws_ami_name = "${var.aws_ami_name}"
   aws_access_key = "${var.aws_access_key}"
   aws_secret_key = "${var.aws_secret_key}"
   aws_region     = "${var.aws_region}"
---
+++ Projects/terraform/create_ami/user_variables.tf
@@ -0,0 +1,57 @@
+
+variable "aws_region" {
+  type = "string"
+  default = "us-west-1"
+  description = "AWS Region"
+}
+
+variable "aws_access_key" {
+  type = "string"
+  default = ""
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+  default = ""
+  description = "AWS Secret key"
+}
+
+variable "aws_ami_name" {
+  type = "string"
+  default = ""
+  description = "AWS AMI Name"
+}
+
+variable "aws_instance_id" {
+  type = "string"
+  default = ""
+  description = "Generated EC2 instance id"
+}
+
+variable "aws_key_name" {
+  type = "string"
+  default = ""
+  description = "AWS Instance Key name"
+}
+
+variable "aws_pem_file" {
+  type="string"
+  default=""
+  description="Private key file(fully qualified path)"
+}
+
+variable "aws_base_ami_id" {
+  type="string"
+  default="ami-f9fb8c96"
+  description="Base ami ID.Defaults to Ubuntu."
+}
+
+variable "aws_base_instance_type" {
+  type="string"
+  default="t2.micro"
+  description="Base ami Type.Defaults to t2.micro."
+}
+
+
+
